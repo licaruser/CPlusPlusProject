4d56f816c8b1b4ace264a31e482136c7a57e3e45
licaruser <liguangyang2021@163.com>
2023-09-02 14:54:53 +0800

    1

--- RradarSignalProcessDemo_Two1031/ThisTestDemo/TestDemo.cpp
+++ RradarSignalProcessDemo_Two1031/ThisTestDemo/TestDemo.cpp
@@ -58,6 +58,162 @@ void Func(vector<int>& Vaule, void(*func)(int))
 		//std::cout << "vaule:" << Pri << std::endl;
 }
 
+class B 
+{
+	int m_data;
+public:
+	B() { cout << "B constructed\n"; }
+	~B() { cout << "B destructed\n"; }
+	B(int i) : m_data(i) 
+	{ 
+		cout << "B constructed with " << i << "\n"; 
+	}
+};
+
+B PlayB(B bb) 
+{ 
+	return bb;
+}
+
+//#include <iostream>
+//#include <exception>
+//using namespace std;
+//class CBase { virtual void dummy() {} };
+//class CDerived : public CBase { int a; };
+//int main() {
+//	try 
+//	{
+//		CBase* pba = new CDerived;
+//		CBase* pbb = new CBase;
+//		CDerived* pd;
+//		pd = dynamic_cast<CDerived*>(pba);
+//		if (pd == 0) cout << "Null pointer on first type-cast " << endl;
+//		pd = dynamic_cast<CDerived*>(pbb);
+//		if (pd == 0) cout << "Null pointer on second type-cast " << endl;
+//	}
+//	catch (exception& e) 
+//	{ 
+//		cout << "Exception: " << e.what(); 
+//	}
+//	return 0;
+//}
+
+#include <iostream>
+#include <vector>
+class Animal {
+public:
+	virtual void eat() const {
+		std::cout << "I eat like a generic Animal. " << std::endl;
+	}
+	virtual ~Animal() {
+	}
+};
+class Wolf : public Animal {
+public:
+	void eat() const {
+		std::cout << "I eat like a wolf!" << std::endl;
+	}
+};
+class Fish : public Animal {
+public:
+	void eat() const {
+		std::cout << "I eat like a fish!" << std::endl;
+	}
+};
+class GoldFish : public Fish {
+public:
+	void eat() const {
+		std::cout << "I eat like a goldfish!" << std::endl;
+	}
+};
+class OtherAnimal : public Animal {
+};
+
+
+//int main() {
+//	std::vector<Animal*> animals;
+//	animals.push_back(new Animal());
+//
+//	animals.push_back(new Wolf());
+//	animals.push_back(new Fish());
+//	animals.push_back(new GoldFish());
+//	animals.push_back(new OtherAnimal());
+//	for (std::vector<Animal*>::const_iterator it = animals.begin(); it != animals.end(); ++it) {
+//		(*it)->eat();
+//		delete* it;
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>          
+//int main()
+//{
+//	char a[] = "hello,world";
+//	char* ptr = a;
+//	printf("%c\n", *(ptr + 4));
+//	printf("%c\n", ptr[4]);
+//	printf("%c\n", a[4]);
+//	printf("%c\n", *(a + 4));
+//	*(ptr + 4) += 1;
+//	printf("%s\n", a);
+//	return 0;
+//}
+
+#include <iostream>
+#include <list>
+//using namespace std;
+//int main()
+//{
+//	list<int> list1;
+//	for (int i = 0; i < 8; i++)
+//	{
+//		list1.push_back(i);
+//
+//	}
+//	for (list<int>::iterator it = list1.begin(); it != list1.end(); ++it)
+//	{
+//		if (*it % 2 == 0)
+//		{
+//			list1.erase(it);
+//		}
+//	}
+//	return 0;
+//}
+
+void swap(int *p1, int* p2)
+{
+	int* p = new int;
+	*p = *p1;
+	*p1 = *p2;
+	*p2 = *p;
+}
+
+
+
+
+void main()
+{
+	int* pp = new int;
+	int* cc = new int;
+	*pp = 2;
+	*cc = 3;
+	swap(pp, cc);
+
+	cout << *pp << "," << *cc << endl;
+}
+
+
+
+//int main()
+//{
+//	PlayB(5);
+//	{
+//		B bb = PlayB(5);
+//		int aa;
+//	}
+//	
+//	return 0;
+//}
 
 
 
@@ -150,93 +306,96 @@ public:
 int value = 5;
 int A::value;
 
-int main()
-{
-	//int value = 10;
-	//A::value++;
-	//value++;
-	//std::cout << ::value << "" << A::value << "" << value << std::endl;
-	//////int howMany = 20;
-	//////int* test = (int*)malloc(howMany * sizeof(int*));
-	//////for (int i = 0; i < howMany; ++i)
-	//////	(test[i]) = i;
-	//////printf("%dn", test[16]);
-	//////remove_element(test, howMany, 16);
-	//////--howMany;
-	//////printf("%dn", test[16]); return 0;
-	/////************************************
-	////Title：自学测试多线程代码
-	////Time：2023.08.01
-	////Author：李广阳
-	////NUmber:001
-	////************************************/
-	////TestDemo *MyClass = new TestDemo;
-	////MyClass->StepAdvance();
-
-
-	//函数指针
-	//vector<int> Vaules = { 1,3,5,8,9,10 };
-	//Func(Vaules,PrinfFunc);   //传入函数中一个函数，可能也就是将指针传进去了
-
-	//继承以及虚函数使用
-	//BaseA Father = SonB EricB;
-	//My_String s1();
-	//BaseA a[5];
-	//std::vector<int> data = { 1,2,3,4 };
-	//for (std::size_t i = 0; i < data.size(); i++)
-	//{
-	//	std::cout << data[i] << std::endl;
-	//}
-	
-
-	//char src[] = "hello,world!";
-	//char dest[20];
-	//stcpy_my(dest, src);
-	//printf("%s\n", dest);
-
-
-	//std::cout << (7 & 3 + 12) << std::endl;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	return 0;
-}
-
+//int main()
+//{
+//	//int value = 10;
+//	//A::value++;
+//	//value++;
+//	//std::cout << ::value << "" << A::value << "" << value << std::endl;
+//	//////int howMany = 20;
+//	//////int* test = (int*)malloc(howMany * sizeof(int*));
+//	//////for (int i = 0; i < howMany; ++i)
+//	//////	(test[i]) = i;
+//	//////printf("%dn", test[16]);
+//	//////remove_element(test, howMany, 16);
+//	//////--howMany;
+//	//////printf("%dn", test[16]); return 0;
+//	/////************************************
+//	////Title：自学测试多线程代码
+//	////Time：2023.08.01
+//	////Author：李广阳
+//	////NUmber:001
+//	////************************************/
+//	////TestDemo *MyClass = new TestDemo;
+//	////MyClass->StepAdvance();
+//
+//
+//	//函数指针
+//	//vector<int> Vaules = { 1,3,5,8,9,10 };
+//	//Func(Vaules,PrinfFunc);   //传入函数中一个函数，可能也就是将指针传进去了
+//
+//	//继承以及虚函数使用
+//	//BaseA Father = SonB EricB;
+//	//My_String s1();
+//	//BaseA a[5];
+//	//std::vector<int> data = { 1,2,3,4 };
+//	//for (std::size_t i = 0; i < data.size(); i++)
+//	//{
+//	//	std::cout << data[i] << std::endl;
+//	//}
+//	
+//
+//	//char src[] = "hello,world!";
+//	//char dest[20];
+//	//stcpy_my(dest, src);
+//	//printf("%s\n", dest);
+//
+//
+//	//std::cout << (7 & 3 + 12) << std::endl;
+//
+//
+//	PlayB();
+//	B(4);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
+//
 
 
 
